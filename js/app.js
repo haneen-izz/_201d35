55e645cbda6107694290a25bf53ca72a7c05cec2
haneen <haneenjaradeh1@gmail.com>
2021-08-01 23:13:29 +0300

    project

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+'use strict';
+let name =prompt("what is your name?");
+alert(" Thinking about getting a cat but not sure which breed is right for you? so welcome to our website we can help you! " +name);
+console.log(name)
+
+let age =Number(prompt("please enter your age"));
+alert(age);
+console.log(age);
+
+let cat =prompt("do you like cats?");
+alert("welcome to my website");
+console.log(cat)
+
+
+let chooseCat =prompt("which cat do you want to know about please choose one of these; Abyssinian Cat , American Shorthair Cat , American Curl Cat Breed ?");
+alert(chooseCat);
+console.log(cat);
